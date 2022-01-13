e64707f88e100981992a0802bc265e52f5a73e20
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2022-01-13 20:28:44 +0200

    check deployment

--- BackendScandiweb/index.php
+++ BackendScandiweb/index.php
@@ -5,17 +5,19 @@ require_once('./Controller/ProductController.php');
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,DELETE");
 
-echo "ok";
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 $productController = new ProductController();
 if ($uri[3] == 'products' && $requestMethod == 'GET') {
+    echo"ok";
     echo $productController->getAllProduct();
 } elseif ($uri[3] == 'products' && $requestMethod == 'POST') {
+    echo"ok";
     $input = (array) json_decode(file_get_contents('php://input'), true);
     echo $productController->createProduct($input);
 } elseif ($uri[3] == 'products' && $requestMethod == 'DELETE') {
+    echo"ok";
     $input =  json_decode(file_get_contents('php://input'), true);
      echo $productController->deleteProduct($input);
 }
