9f2374b873cc08a3cab3d564e7504a4bd4646672
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2022-01-13 20:18:54 +0200

    check sucess deploy

--- BackendScandiweb/index.php
+++ BackendScandiweb/index.php
@@ -5,7 +5,7 @@ require_once('./Controller/ProductController.php');
 header("Content-Type: application/json; charset=UTF-8");
 header("Access-Control-Allow-Methods: OPTIONS,GET,POST,DELETE");
 
-
+echo "ok";
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode('/', $uri);
 $requestMethod = $_SERVER["REQUEST_METHOD"];
