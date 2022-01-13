e49d4f932a372bb57a7c258c08c45b8c519bdb32
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2022-01-13 20:51:22 +0200

    check deploy

--- BackendScandiweb/index.php
+++ BackendScandiweb/index.php
@@ -14,7 +14,7 @@ if ($requestMethod == 'GET') {
 } elseif ($requestMethod == 'POST') {
     echo"i am post";
     $input = (array) json_decode(file_get_contents('php://input'), true);
-    echo $input;
+    echo $input['name'];
     echo $productController->createProduct($input);
 } elseif ($requestMethod == 'DELETE') {
     echo" i am delete";
