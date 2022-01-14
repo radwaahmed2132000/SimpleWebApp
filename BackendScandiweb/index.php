852bd3514c2dcffaf39d650f90a365a543f7010d
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2022-01-14 16:03:45 +0200

    connect to db

--- BackendScandiweb/index.php
+++ BackendScandiweb/index.php
@@ -14,7 +14,6 @@ if ($requestMethod == 'GET') {
 } elseif ($requestMethod == 'POST') {
     echo"i am post";
     $input = (array) json_decode(file_get_contents('php://input'), true);
-    echo $input['name'];
     echo $productController->createProduct($input);
 } elseif ($requestMethod == 'DELETE') {
     echo" i am delete";
