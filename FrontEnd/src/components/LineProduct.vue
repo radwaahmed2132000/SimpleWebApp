50e0c55e35e73d7d0c27d74ddc8dfc6e130b82e1
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2022-01-15 14:24:47 +0200

    add front

---
+++ FrontEnd/src/components/LineProduct.vue
@@ -0,0 +1,23 @@
+<template>
+     <div class="container-fluid m-3">
+          <div class="row" id="line">
+              </div>    
+        </div>  
+</template>
+<script>
+export default ({
+    name: 'LineProduct'
+
+})
+</script>
+
+<style >
+#line
+{
+    position: relative;
+    left: 7%;
+    width: 87%;
+    height: 1px;
+    background-color: black;
+}
+</style>
---
+++ FrontEnd/src/components/Product.vue
@@ -0,0 +1,212 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-8">
+                      <h2>Product List</h2>
+                </div>
+                <div class="col-lg-4 buttons">
+                    <b-button >
+                         <router-link to="/AddProduct">
+                            ADD
+                        </router-link>
+                    </b-button>
+                     
+                    <b-button v-on:click="deleteProduct()" id="delete-product-btn">MASS DELETE</b-button>
+                </div>
+             
+    
+            </div>
+        </div>
+       
+       <LineProduct/>
+           
+               <div class="container mb-5">
+                   <div class="row">
+                      
+                       <div class="col-lg-3 my-2"  v-for="product in products" :key="product.id"  >
+                           
+                         <b-card-group >  
+                           <b-card  >  
+                                <b-form-checkbox class="delete-checkbox"
+                           
+                            v-model="ids"
+                            :value="product.id"
+                        >
+                    
+                        </b-form-checkbox>
+                        <br>
+                        <b-card-text v-if="product.type=='Furniture'">
+                          {{product.SKU}} <br>
+                          {{product.name}}<br>
+                         {{product.price}} $ <br>
+
+                          Dimension: {{product.width}}x{{product.height}}x{{product.length}}<br>
+                        </b-card-text>
+                          <b-card-text v-if="product.type=='DVD'">
+                          {{product.SKU}} <br>
+                          {{product.name}}<br>
+                          {{product.price}} $ <br>
+
+                          Size: {{product.size}} MB<br>
+                        </b-card-text>
+                         <b-card-text v-if="product.type=='Book'">
+                          {{product.SKU}} <br>
+                          {{product.name}}<br>
+                          {{product.price}} $ <br>
+
+                          Weight: {{product.weight}}KG<br>
+                        </b-card-text>
+
+                           <br>
+                          </b-card>
+                           </b-card-group >
+                       </div>
+                    
+            
+                   </div>
+               </div>
+                
+                
+           
+    <LineProduct/>
+    <h4>
+        Scandiweb Test Assignment
+    </h4>
+     
+       
+    </div> 
+
+</template>
+<script>
+
+import LineProduct from './LineProduct.vue';
+import axios from 'axios';
+
+export default ({
+    name: 'Product',
+    components: {
+     LineProduct,
+ 
+   },
+    data : function () {
+         return {
+             /**
+              * all products 
+              * @value array of product
+              */
+               products :[] ,
+               /**
+                * number of products
+                */
+               count:0,
+               /**
+                * ids of selected products
+                */
+               ids : [],
+               data: [],
+               status:"",
+               deletedIds: []
+               
+         };
+    },
+    methods :{
+        /**
+         * get all products
+         * @return void
+         */
+       getAllProducts : function()
+       {
+           this.products =[];
+          
+            axios.get('http://127.0.0.1/BackendScandiweb/index.php/products')
+           .then(response=>
+               { 
+                    this.data = response.data.response;
+                    this.status= response.data.meta.status;
+                    if(this.status=="200"){
+                            this.count =this.data.length;
+    
+                    for(let i=0; i<this.count ;i++) {
+                            this.products.push(
+                                {
+                                name: this.data[i].name,
+                                SKU:this.data[i].sku,
+                                type: this.data[i].type,
+                                price :this.data[i].price,
+                                width :this.data[i].width ,
+                                height:this.data[i].height,
+                                length:this.data[i].length,
+                                size:this.data[i].size,
+                                weight:this.data[i].weight,
+                                id :String(this.data[i].id)
+                                 }
+                            );
+                    }
+                    }    
+            
+           });
+    
+        
+           
+         
+
+       },
+       /**
+        * delete  selected products
+        * @return void
+        */
+      deleteProduct : function () {
+           
+           for (let i=0;i< this.ids.length;i++){
+                // console.log(this.ids[i]);
+                this.deletedIds.push(
+                    {
+                        "id":this.ids[i]
+                    }
+                )
+           }
+      
+           const headers = { 
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+           };
+        //    console.log(JSON.stringify(this.deletedIds));
+            axios.post("http://127.0.0.1/BackendScandiweb/index.php/del",JSON.stringify(this.deletedIds),{headers});
+    
+           this.deletedIds=[];
+             this.getAllProducts();
+       }
+       
+    },
+    
+  mounted () {
+    this.getAllProducts()
+}
+ , 
+})
+</script>
+<style  >
+*{
+    margin: 0%;
+    padding: 0%;
+    color:black;
+}
+
+h2
+{
+    margin-right:30%;
+    
+}
+#delete-product-btn{
+  
+    margin-left:20% ;
+}
+
+button{
+    background: blue;
+}
+
+
+</style>
+
