56e3df5fead36b0b39527c3ad99bb98ef18d3c4d
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2022-01-16 21:21:49 +0200

    update connection with localhost

--- BackendScandiweb/Model/connection.php
+++ BackendScandiweb/Model/connection.php
@@ -3,9 +3,9 @@
 class DBConnection
 {
     public $serverName = 'localhost'; // server ip
-    public $dbUserName = 'id18281151_radwa'; // server acess username
-    public $dbUserNamePass = '<l~S47Rjh!ws~*~_';      // the password of the specified username, in this case there's no password, http://localhost/phpmyadmin/server_privileges.php?viewing_mode=server formore info
-    public $dbName = 'id18281151_db_task';     // database name
+    public $dbUserName = 'root';      // server acess username
+    public $dbUserNamePass = '';      // the password of the specified username, in this case there's no password, http://localhost/phpmyadmin/server_privileges.php?viewing_mode=server formore info
+    public $dbName = 'testapp';     // database name
     private $dbConnection;
     public static $instance;
 
