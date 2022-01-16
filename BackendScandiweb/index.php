809b8940390ca2cc428f6406633511d5638cfe68
radwaahmed2132000 <56734728+radwaahmed2132000@users.noreply.github.com>
2022-01-16 21:20:53 +0200

    update backend

--- BackendScandiweb/index.php
+++ BackendScandiweb/index.php
@@ -2,27 +2,20 @@
 
 require_once('./Controller/ProductController.php');
 
-header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: POST");
-header("Access-Control-Max-Age: 3600");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Methods: OPTIONS,GET,POST,DELETE");
+
+
 $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-
 $uri = explode('/', $uri);
-
 $requestMethod = $_SERVER["REQUEST_METHOD"];
 $productController = new ProductController();
-
-
-if ($requestMethod == 'GET') {
+if ($uri[3] == 'products' && $requestMethod == 'GET') {
     echo $productController->getAllProduct();
-} elseif ($requestMethod == 'POST' and $uri[2] == 'add') {
+} elseif ($uri[3] == 'products' && $requestMethod == 'POST') {
     $input = (array) json_decode(file_get_contents('php://input'), true);
     echo $productController->createProduct($input);
-    // here should be delete method not post method but free version not support delete
-} elseif ($requestMethod == 'POST' and $uri[2] == 'del') {
+} elseif ($uri[3] == 'products' && $requestMethod == 'DELETE') {
     $input =  json_decode(file_get_contents('php://input'), true);
      echo $productController->deleteProduct($input);
 }
-// https://scandiwebtasg.000webhostapp.com/index.php/del
